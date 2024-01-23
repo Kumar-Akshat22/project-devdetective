d85bec48c7edcf944f4232c42a8bf7a56f447cb4
Kumar Akshat <kakshat247@gmail.com>
2024-01-23 23:30:24 +0530

    implemented the API call functionality

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log('Hello, World');
+// Varialbles
+const url = "https://api.github.com/users/";
+const input = document.getElementById("input");
+const search = document.getElementById("search");
+
+
+search.addEventListener('click' , ()=>{
+    
+    console.log('Printing the search input value');
+    getUserData(url + input.value);
+})
+
+async function getUserData(newUrl){
+
+    try{
+
+        const res = await fetch(newUrl);
+        const data = await res.json();
+        console.log(data);
+
+    }
+
+    catch(error){
+
+        console.log('Error in fetching the data');
+
+    }
+}
+
