944f178e683ccc53a986f724285d1d1bafbce23c
Kumar Akshat <kakshat247@gmail.com>
2024-01-24 20:12:00 +0530

    updated the functionality of displaying some more data

--- script.js
+++ script.js
@@ -5,7 +5,15 @@ const search = document.getElementById("search");
 const user_avataar = document.getElementById("user_avataar");
 const user_name = document.getElementById("user_name");
 const date_joined = document.getElementById("date_joined");
-const user_profile = document.getElementById("user_profile")
+const user_profile = document.getElementById("user_profile");
+const user_description = document.getElementById("user_description");
+const repo_count = document.getElementById("repo_count");
+const follower_count = document.getElementById("follower_count");
+const following_count = document.getElementById("following_count");
+const user_location = document.getElementById("user_location");
+const user_website = document.getElementById("user_website");
+const user_twitter = document.getElementById("user_twitter");
+const user_company = document.getElementById("user_company");
 
 search.addEventListener('click' , ()=>{
     
@@ -37,7 +45,13 @@ function renderUserInfo(userData){
     user_name.innerText = `${userData.name}`;
     user_profile.href = `${userData.html_url}`;
     user_profile.innerText = `${userData.login}`;
-
+    user_description.innerText = `${userData.bio}`;
+    repo_count.innerText = `${userData.public_repos}`;
+    follower_count.innerText = `${userData.followers}`;
+    following_count.innerText = `${userData.following}`;
+    user_location.innerText = (userData.location === null) ? "Not Available" : `${userData.location}`;
+    user_company.innerText = (userData.company === null) ? "Not Available" : `${userData.comapny}`;
+    
 
 }
 
